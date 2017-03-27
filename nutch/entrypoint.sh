1a18e1ba284bca0196930f7921b3741ffa742c66
Adam Hatherly <adam.hatherly@nhs.net>
2017-03-27 18:03:23 +0100

    Updated nutch to run 5 iterations

--- nutch/entrypoint.sh
+++ nutch/entrypoint.sh
@@ -2,6 +2,6 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/
 cd /root/nutch
-bin/crawl ./urls ./crawl 1
+bin/crawl ./urls ./crawl 5
 bin/nutch solrindex http://solr-developernetwork:8983/solr/developernetwork crawl/crawldb/ -linkdb crawl/linkdb/ crawl/segments/* -filter -normalize -deleteGone
 
