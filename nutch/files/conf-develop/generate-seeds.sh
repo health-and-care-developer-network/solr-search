45c5088e057c927043bfe10d57356c10f26398d6
Adam Hatherly <adam.hatherly@nhs.net>
2018-03-03 09:37:16 +0000

    Updated parsing rules for fhir server pages

--- nutch/files/conf-develop/generate-seeds.sh
+++ nutch/files/conf-develop/generate-seeds.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Simple script to scrape all URLs from the API hub page and add them to the seed file, so we make sure we are indexing all API specs
-wget --convert-links http://172.17.0.1:8080/apis.html -O /root/nutch/apihub.htm
-sed -n 's/.*href="\([^"]*\).*/\1/p' /root/nutch/apihub.htm >> /root/nutch/urls/seed.txt
+#wget --convert-links http://172.17.0.1:8080/apis.html -O /root/nutch/apihub.htm
+#sed -n 's/.*href="\([^"]*\).*/\1/p' /root/nutch/apihub.htm >> /root/nutch/urls/seed.txt
 
 # De-duplicate the seed file
-awk '!seen[$0]++' /root/nutch/urls/seed.txt > /root/nutch/urls/seed2.txt
-rm /root/nutch/urls/seed.txt
-mv /root/nutch/urls/seed2.txt /root/nutch/urls/seed.txt
+#awk '!seen[$0]++' /root/nutch/urls/seed.txt > /root/nutch/urls/seed2.txt
+#rm /root/nutch/urls/seed.txt
+#mv /root/nutch/urls/seed2.txt /root/nutch/urls/seed.txt
 
